77c7454db18798887630cedcb2d6be7b94845f86
すみsann <email.smithfield@gmail.com>
2022-09-12 09:06:10 +0900

    Update main.py

--- VRChatOSC/main.py
+++ VRChatOSC/main.py
@@ -5,10 +5,10 @@ import requests
 
 server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 9001), dispatcher)
 print("Serving on {}".format(server.server_address))
-
-if 0 == 1:
-    response = requests.get('http://mimihp.local/?LENGTH0=1000&POWER0=255&LENGTH1=1000&POWER1=255')
-    print(response.status_code)    # HTTPのステータスコード取得
-    print(response.text)    # レスポンスのHTMLを文字列で取得
+url = 'http://mimihp.local/?LENGTH=' + length0 + '&POWER=' + power0 + '&LENGTH1' + length1 + '&POWER1' + power1
+print (url)
+response = requests.get(url)
+print(response.status_code)    # HTTPのステータスコード取得
+print(response.text)    # レスポンスのHTMLを文字列で取得
 
 server.serve_forever()
