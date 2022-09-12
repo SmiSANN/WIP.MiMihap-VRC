cef810054fc1051ece3adaaffd392601851c0cfe
すみsann <email.smithfield@gmail.com>
2022-09-12 09:07:46 +0900

    Update main.py

--- VRChatOSC/main.py
+++ VRChatOSC/main.py
@@ -5,7 +5,9 @@ import requests
 
 server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 9001), dispatcher)
 print("Serving on {}".format(server.server_address))
-url = 'http://mimihp.local/?LENGTH=' + length0 + '&POWER=' + power0 + '&LENGTH1' + length1 + '&POWER1' + power1
+
+
+url = 'http://mimihp.local/?LENGTH=' + length0 + '&POWER=' + power0 + '&LENGTH1=' + length1 + '&POWER1=' + power1
 print (url)
 response = requests.get(url)
 print(response.status_code)    # HTTPのステータスコード取得
